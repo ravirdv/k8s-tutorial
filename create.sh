b3a89125a0ffb2a00b73d20505331bb7e85ee83d
Ravi VAGADIA <r.vagadia@f5.com>
2019-04-23 03:35:33 -0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,9 +1,9 @@
-for i in {1..5}
+for i in {1..$1}
 do
 mkdir /tmp/user-$i
 cp docker-compose.yml /tmp/user-$i/
 port=$((5000 + $i))
 echo $port
 sed -i "s/5000/$port/g" /tmp/user-$i/docker-compose.yml
-docker-compose -f /tmp/user-$i/docker-compose.yml up -d  --scale node=2
+docker-compose -f /tmp/user-$i/docker-compose.yml up -d  --scale node=3
 done
